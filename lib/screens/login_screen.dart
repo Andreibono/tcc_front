50fe666ce84c5ba1b54f79d6de4ce502079d62bf
Bruno Fonseca <brunomaiaf20@gmail.com>
2023-05-25 15:43:17 -0400

    melhora na pagina de login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,23 +6,44 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      child: ListView(children: [
-        Column(children: [
-          Container(
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.fromLTRB(0, 80, 0, 200),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(100), color: Colors.grey),
-          ),
-          Column(
-            children: [
-              AuthForm(),
-            ],
-          )
-        ]),
-      ]),
-    ));
+      body: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    child: Column (
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          decoration: 
+                          BoxDecoration(borderRadius: BorderRadius.circular(50), color: Colors.grey),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    child: Column (
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        AuthForm()
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      )
+    );
   }
 }
