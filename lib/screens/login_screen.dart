e621786dd202527145b43b8491de733f95e45753
Bruno Fonseca <brunomaiaf20@gmail.com>
2023-05-25 15:46:17 -0400

    add padding e refatorando

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,43 +6,40 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    child: Column (
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Container(
-                          width: 100,
-                          height: 100,
-                          decoration: 
-                          BoxDecoration(borderRadius: BorderRadius.circular(50), color: Colors.grey),
-                        )
-                      ],
-                    ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Column (
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: 
+                      BoxDecoration(borderRadius: BorderRadius.circular(50), color: Colors.grey),
+                    )
+                  ],
+                ),
+              ),
+              const Expanded(
+                flex: 2,
+                child:  Padding(
+                  padding:  EdgeInsets.only(bottom: 10),
+                  child:  Column (
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      AuthForm()
+                    ],
                   ),
                 ),
-                Expanded(
-                  flex: 2,
-                  child: Container(
-                    child: Column (
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        AuthForm()
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ],
-        ),
+              )
+            ],
+          ),
+        ],
       )
     );
   }
